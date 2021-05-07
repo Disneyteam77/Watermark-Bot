8d87d90b5c6b22d9b2ef7ffc1529dc7e6dd50f8a
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-05-07 19:39:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ async def HelpWatermark(bot, cmd):
 	await cmd.reply_text(
 		text=Config.USAGE_WATERMARK_ADDER,
 		parse_mode="Markdown",
-		reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005"), InlineKeyboardButton("Support Group", url="https://t.me/linux_repo")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")], [InlineKeyboardButton("Source Code", url="https://github.com/AbirHasan2005/Watermark-Bot")]]),
+		reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/Amalbiju154"), InlineKeyboardButton("Support Group", url="https://t.me/Yeageristbots")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Animemusicarchive6")], [InlineKeyboardButton("Botlogs", url="https://t.me/watermarkbotlogs")]]),
 		disable_web_page_preview=True
 	)
 
@@ -446,7 +446,7 @@ async def button(bot, cmd: CallbackQuery):
 				return
 			except Exception:
 				await cmd.message.edit(
-					text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+					text="Something went Wrong. Contact my [Support Group](https://t.me/Yeageristbots).",
 					parse_mode="markdown",
 					disable_web_page_preview=True
 				)
@@ -454,7 +454,7 @@ async def button(bot, cmd: CallbackQuery):
 		await cmd.message.edit(
 			text=Config.USAGE_WATERMARK_ADDER,
 			parse_mode="Markdown",
-			reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005"), InlineKeyboardButton("Support Group", url="https://t.me/linux_repo")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]]),
+			reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/Amalbiju154"), InlineKeyboardButton("Support Group", url="https://t.me/Yeageristbots")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Animemusicarchive6")]]),
 			disable_web_page_preview=True
 		)
 
@@ -471,7 +471,7 @@ async def button(bot, cmd: CallbackQuery):
 				user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.message.chat.id)
 				if user.status == "kicked":
 					await cmd.message.edit(
-						text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+						text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/Yeageristbots).",
 						parse_mode="markdown",
 						disable_web_page_preview=True
 					)
@@ -494,7 +494,7 @@ async def button(bot, cmd: CallbackQuery):
 				return
 			except Exception:
 				await cmd.message.edit(
-					text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+					text="Something went Wrong. Contact my [Support Group](https://t.me/Yeageristbots).",
 					parse_mode="markdown",
 					disable_web_page_preview=True
 				)
